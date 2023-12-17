688e3efae685317f66ced8d109b6b377a705e702
apdelsm <apdelsm@gmail.com>
2023-12-17 16:15:52 +0100

    make the worker get to an url every 5 seconds

--- worker/app.js
+++ worker/app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const axios = require('axios');
+
 var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -11,9 +13,18 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function makeGetRequest(url) {
+  try {
+    const response = await axios.get(url);
+    console.log(`Response from ${url}:`, response.data);
+  } catch (error) {
+    console.error(`Error making GET request to ${url}:`, error.message);
+  }
+}
+
 async function main() {
   while (true) {
-    console.log("This is the first message");
+    await makeGetRequest('http://localhost:3000/');
     await delay(5000);
   }
 }
