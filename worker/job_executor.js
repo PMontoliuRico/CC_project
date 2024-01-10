604a3e8c05f12356a1a29eb02d34fe8ecb7a8bf8
apdelsm <apdelsm@gmail.com>
2024-01-10 13:18:09 +0100

    Add logic to clone repo

--- worker/job_executor.js
+++ worker/job_executor.js
@@ -1,4 +1,18 @@
 const NATS = require('nats');
+const shell = require('shelljs')
+
+const jobExample = {
+  "source": "https://github.com/apdelsm/cc_test_1.git",
+  "parameters": [1, 2]
+};
+
+async function executeJob(job) {
+  const path = __dirname + "/job";
+  shell.mkdir(path);
+  shell.cd(path);
+  shell.exec(`git clone ${job.source} .`);
+  //shell.rm('-rf', path);
+}
 
 async function main() {
   const natsUrl = 'nats://localhost:4222';
@@ -10,13 +24,12 @@ async function main() {
   const sub = nc.subscribe("jobs_executors", {queue: groupName });
   (async () => {
     for await (const m of sub) {
-      console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+      executeJob(JSON.parse(sc.decode(m.data)));
     }
     console.log("subscription closed");
   })();
 
-  nc.publish("jobs_executors", sc.encode("world"));
-  nc.publish("jobs_executors", sc.encode({"again":1}));
+  nc.publish("jobs_executors", sc.encode(JSON.stringify(jobExample)));
 }
 
 main();
