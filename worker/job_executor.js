cb49b83f47edf0a71f1f2722296b7360f32c5626
apdelsm <apdelsm@gmail.com>
2024-01-16 13:42:11 +0100

    Add updates in KV store and standarize use of quotation marks

--- worker/job_executor.js
+++ worker/job_executor.js
@@ -1,37 +1,34 @@
 const NATS = require('nats');
 const shell = require('shelljs')
 
-const jobExample = {
-  "source": "https://github.com/apdelsm/cc_test_1.git",
-  "parameters": [1, 2]
-};
-
-async function executeJob(job) {
-  const path = __dirname + "/job";
+async function executeJob(job, kv, sc) {
+  await kv.put(`${job.user}.${job.id}`, sc.encode('Running'));
+  const path = __dirname + '/job';
   shell.mkdir(path);
   shell.cd(path);
   result = shell.exec(`git clone ${job.source} .`);
   if(result.code !== 0) {
     //job can't exec bc repo
-    //TODO: send to KV that it state is failed
     //TODO: send to object store the error msje
+    await kv.put(`${job.user}.${job.id}`, sc.encode('Failed'));
     console.log(result.stderr)
   }
-  parameters = " "
+  parameters = ' '
   for(i in job.parameters) {
     parameters += `${job.parameters[i]} `;
   }
   result = shell.exec(`./run.sh${parameters}`);
   if(result.code == 0) {
+    console.log(`${job.user}.${job.id}`)
     //job executed succesfully
-    //TODO: send to KV that it state is completed
-    //TODO: send the result to object store 
+    //TODO: send the result to object store
+    await kv.put(`${job.user}.${job.id}`, sc.encode('Completed')); 
     console.log(result.stdout);
   }
   else {
     //job failed
-    //TODO: send to KV that it state is failed
     //TODO: send to object store the error msje
+    await kv.put(`${job.user}.${job.id}`, sc.encode('Failed'));
     console.log(result.stderr);
   }
   
@@ -39,21 +36,26 @@ async function executeJob(job) {
 }
 
 async function main() {
-  const natsUrl = 'nats://localhost:4222';
-
-  const nc = await NATS.connect({ url: natsUrl });
-  console.log(`connected to ${nc.getServer()}`);
+  const natskvUrl = 'nats://localhost:4225';
+  const natsQueueUrl = 'nats://localhost:4222';
+  const ncjs = await NATS.connect({ servers: [natskvUrl] });
+  console.log(`connected to ${ncjs.getServer()}`);
+  const ncq = await NATS.connect({ url: natsQueueUrl });
+  console.log(`connected to ${ncq.getServer()}`);
+  const js = await ncjs.jetstream();
+  const kv = await js.views.kv('testing', { history: 5 });
   const sc = NATS.StringCodec();
-  groupName  = "group1"
-  const sub = nc.subscribe("jobs_executors", {queue: groupName });
+
+  
+  groupName  = 'group1'
+  const sub = ncq.subscribe('jobs_executors', {queue: groupName });
   (async () => {
     for await (const m of sub) {
-      executeJob(JSON.parse(sc.decode(m.data)));
+      executeJob(JSON.parse(sc.decode(m.data)), kv, sc);
     }
-    console.log("subscription closed");
+    console.log('subscription closed');
   })();
 
-  nc.publish("jobs_executors", sc.encode(JSON.stringify(jobExample)));
 }
 
 main();
