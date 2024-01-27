0853ca619be47906c5f0cc8621bdf849b9bb7e1f
eddypmr <eddypmorochor@gmail.com>
2024-01-27 13:45:31 +0100

    observer v1
    Añado cambios para observer

--- observer/observer.js
+++ observer/observer.js
@@ -1,60 +1,70 @@
 const nats = require('nats');
 const axios = require('axios');
+const cron = require('node-cron');
+const http = require('http');
+const socketIo = require('socket.io');
 
+async function obtenerDatosCola() {
+  try {
+    const nc = await nats.connect({
+      servers: ['localhost:4222'],
+    });
 
-(async () =>{
-    try{
-      const nc = await nats.connect({
-        servers: ['localhost:4222'],
-      })
-      console.log("Establecemos conexion con el monitor del servidor nats");
+    console.log("Establecemos conexión con el monitor del servidor NATS");
 
+    setInterval(async () => {
+      try {
+        // Utilizando axios para hacer la solicitud HTTP para obtener datos de la cola de mensajes
+        const data_connz = await axios.get("http://localhost:8222/connz");
+        const data_varz = await axios.get("http://localhost:8222/varz");
 
-      // Utilizando axios para hacer la solicitud HTTP para obtener datos de la cola de mensajes
-      const data_connz = await axios.get("http://localhost:8222/connz");
-      const data_varz = await axios.get("http://localhost:8222/varz");
-      if(data_connz.data != undefined){
-        data = data_connz.data;
-        datos = "";
-        //Comprobamos el numero de conexiones que hay en el servidor
-        if(data.num_connections != undefined & data.limit != undefined & data.num_connections < data.limit){
-          datos  = datos +"Estado del servidor: "+ data_varz.status + " " + data_varz.statusText + ".";
+        if (data_connz.data != undefined) {
+          const datos = [];
 
-          conexiones = data.num_connections;
-          datos  = datos +"\nNúmero de conexiones: "+ conexiones + ".";
-          //añadimos un aviso para cuando se este acercando al limite de conexiones definidas
-          if(conexiones == (data.limit*0.75)){console.log("El número de conexiones está llegando a su límite " + data.limit + ".");}
-        }else{
-          console.log("El número de conexiones ha llegado a su limite.");
-        }
+          // Comprobamos el número de conexiones que hay en el servidor
+          if (data_connz.data.num_connections != undefined &&
+              data_connz.data.limit != undefined &&
+              data_connz.data.num_connections < data_connz.data.limit) {
 
-        if(data_varz != undefined){
-          //console.log(data_varz.data);
-          data = data_varz.data;
-          datos = datos + "\nMemoria del servidor NATS: " + data.mem + ".";
+            datos.push("Estado del servidor: " + data_varz.status + " " + data_varz.statusText + ".");
+            const conexiones = data_connz.data.num_connections;
+            datos.push("Número de conexiones: " + conexiones + ".");
 
-          retrasos = data.slow_consumers;
-          if(retrasos > 0 ){
-            datos = datos + "\nCuidado!!! Hay " + retrasos + " consumidores con retrasos en la cola de mensajes.";
-          }else if(retrasos == 0){
-            datos = datos + "\nNo hay ningun consumidor con retrasos en la cola de mensajes.";
+            // Añadimos un aviso para cuando se esté acercando al límite de conexiones definidas
+            if (conexiones === Math.floor(data_connz.data.limit * 0.75)) {
+              console.log("El número de conexiones está llegando a su límite " + data_connz.data.limit + ".");
+            }
+          } else {
+            console.log("El número de conexiones ha llegado a su límite.");
           }
 
+          if (data_varz.data != undefined) {
+            datos.push("Memoria del servidor NATS: " + data_varz.data.mem + ".");
+
+            const retrasos = data_varz.data.slow_consumers;
+            if (retrasos > 0) {
+              datos.push("¡Cuidado! Hay " + retrasos + " consumidores con retrasos en la cola de mensajes.");
+              console.log("Sería necesario el despliegue de otro worker");
+            } else if (retrasos === 0) {
+              datos.push("No hay ningún consumidor con retrasos en la cola de mensajes.");
+            }
+          }
+
+          console.log("---------------------\nDATOS DE LA COLA DE MENSAJES\n" + datos.join("\n"));
+
         }
-
-
-
-
-        //Habría que ver el numero de workers y clientes si publican en una cola un numero de mensaje
+      } catch (error) {
+        console.log(error.message);
       }
+    }, 30000); // Ejecutar cada 30 segundos
 
-      console.log("---------------------\nDATOS DE LA COLA DE MENSAJES\n" + datos);
+    // Esperar a que el programa se detenga
+    await new Promise(resolve => setInterval(resolve, 2147483647));
+    await nc.close();
 
-      /*$.getJSON("http://localhost:8222/connz", function (data) {
-        console.log(data);
-      });*/
-      await nc.close();
-    }catch(err){
-      console.log(err.message);
-    }
-  })();
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+obtenerDatosCola();
