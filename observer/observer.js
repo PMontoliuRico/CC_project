c6ff1ae0663e67c83f2f117eb8c3548e46e45ed3
eddypmr <eddypmorochor@gmail.com>
2024-01-30 22:17:24 +0100

    version observer final
    añadimos la version final del observer

--- observer/observer.js
+++ observer/observer.js
@@ -1,69 +1,77 @@
 const nats = require('nats');
-const axios = require('axios');
-const cron = require('node-cron');
 const http = require('http');
-const socketIo = require('socket.io');
+
+global.inc_jobs=0;
+global.inc_jobsprocessed=0;
+global.contador=0;
 
 async function obtenerDatosCola() {
   try {
     setInterval(async () => {
       try {
-        // Utilizando axios para hacer la solicitud HTTP para obtener datos de la cola de mensajes
-        const data_connz = await axios.get("http://127.0.0.0:8222/connz");
-        const data_varz = await axios.get("http://127.0.0.0:8222/varz");
-        console.log("Establecemos conexión con el monitor del servidor NATS");
-    
-
-        if (data_connz.data != undefined) {
-          const datos = [];
-
-          // Comprobamos el número de conexiones que hay en el servidor
-          if (data_connz.data.num_connections != undefined &&
-              data_connz.data.limit != undefined &&
-              data_connz.data.num_connections < data_connz.data.limit) {
-
-            datos.push("Estado del servidor: " + data_varz.status + " " + data_varz.statusText + ".");
-            const conexiones = data_connz.data.num_connections;
-            datos.push("Número de conexiones: " + conexiones + ".");
-
-            // Añadimos un aviso para cuando se esté acercando al límite de conexiones definidas
-            if (conexiones > Math.floor(data_connz.data.limit * 0.75)) {
-              console.log("El número de conexiones está llegando a su límite " + data_connz.data.limit + ".");
-            }else if(conexiones === Math.floor(data_connz.data.limit)) {
-              console.log("El número de conexiones ha llegado a su límite.");
-            }else if(conexiones < Math.floor(data_connz.data.limit * 0.1)){
-              console.log("El número de conexiones es muy bajo. Se podría reducir el número de workers");
-            }
-
-          }
-
-          if (data_varz.data != undefined) {
-            datos.push("Memoria del servidor NATS: " + data_varz.data.mem + ".");
-
-            const retrasos = data_varz.data.slow_consumers;
-            if (retrasos > 0) {
-              datos.push("¡Cuidado! Hay " + retrasos + " consumidores con retrasos en la cola de mensajes.");
-              console.log("Sería necesario el despliegue de otro worker");
-            } else if (retrasos === 0) {
-              datos.push("No hay ningún consumidor con retrasos en la cola de mensajes.");
-            }
-          }
-
-          console.log("---------------------\nDATOS DE LA COLA DE MENSAJES\n" + datos.join("\n"));
-
+        const temp_inc_jobs = global.inc_jobs;
+        const temp_inc_jobsprocessed = global.inc_jobsprocessed;
+        const diff = temp_inc_jobs - temp_inc_jobsprocessed;
+        forget = false;
+        if(diff > 5 && (temp_inc_jobs * 0.25) > temp_inc_jobsprocessed){
+          console.log("Hay una diferencia significativa de trabajos procesados y número de trabajos totales.\nEs necesario el incremento de un worker.");
+          global.contador = 0;
+          forget = true;
         }
+        else if ((temp_inc_jobs * 0.9) <= temp_inc_jobsprocessed) {
+          global.contador++;
+          forget = false;
+          if (global.contador > 10) {
+            console.log("Hay poca carga en el sistema, se puede desactivar un worker.");
+            global.contador = 0;
+            forget = true;
+          } else {
+            console.log('Carga baja, analizando si es posible disminuir un worker');
+          }
+        } else {
+          console.log("los workers estan bien");
+          global.contador = 0;
+          forget = true;
+        }
+        if (forget) {
+          global.inc_jobs -= temp_inc_jobsprocessed;
+          global.inc_jobsprocessed -= temp_inc_jobsprocessed;
+        }
+        
       } catch (error) {
         console.log(error.message);
       }
-    }, 5000); // Ejecutar cada 5 segundos
 
-    // Esperar a que el programa se detenga
-    await new Promise(resolve => setInterval(resolve, 2147483647));
-    await nc.close();
+    }, 5000); // Ejecutar cada 5 segundos
 
   } catch (err) {
     console.log(err.message);
   }
 }
 
-obtenerDatosCola();
+async function main(){
+  const natsQueueUrls = ['nats://natsq-0:4222', 'nats://natsq-1:4223', 'nats://natsq-2:4224'];
+  const ncq = await nats.connect({ servers: natsQueueUrls });
+  console.log(`connected to ${ncq.getServer()}`);
+
+  const sub1 = ncq.subscribe('jobs_executors');
+  const sub2 =  ncq.subscribe('worker_monitor');
+
+  (async () => {
+    for await (const m of sub1) {
+      global.inc_jobs++;
+    }
+    console.log('subscription closed');
+  })();
+
+  (async () => {
+    for await (const m of sub2) {
+      global.inc_jobsprocessed++;
+    }
+    console.log('subscription closed');
+  })();
+  
+  obtenerDatosCola();
+}
+
+main()
